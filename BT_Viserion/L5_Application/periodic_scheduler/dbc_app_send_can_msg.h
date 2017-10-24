c5a0c97beeb57a4ab562d2dcf1ba534955354709
Dheemanth B <dheemanth10@gmail.com>
2017-10-23 22:26:06 -0700

    Complted HeartBeat Changes

---
+++ BT_Viserion/L5_Application/periodic_scheduler/dbc_app_send_can_msg.h
@@ -0,0 +1,16 @@
+/*
+ * dbc_app_send_can_msg.h
+ *
+ *  Created on: 24-Oct-2017
+ *      Author: pratap
+ */
+
+#ifndef L5_APPLICATION_PERIODIC_SCHEDULER_DBC_APP_SEND_CAN_MSG_H_
+#define L5_APPLICATION_PERIODIC_SCHEDULER_DBC_APP_SEND_CAN_MSG_H_
+
+#include <stdint.h>
+
+bool dbc_app_send_can_msg(uint32_t mid, uint8_t dlc, uint8_t bytes[8]);
+
+
+#endif /* L5_APPLICATION_PERIODIC_SCHEDULER_DBC_APP_SEND_CAN_MSG_H_ */
---
+++ Sensors_Viserion/pre_build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# 
+# This file executes from the build directory by the Eclipse "Pre Build" step
+#
+
+python ../_can_dbc/dbc_parse.py -i ../_can_dbc/243.dbc -s DRIVER > ../_can_dbc/generated_can.h
+python ../../dbc_parse.py -i ../../Viserion.dbc -s SENSORS > ../_can_dbc/generated_Viserion.h
