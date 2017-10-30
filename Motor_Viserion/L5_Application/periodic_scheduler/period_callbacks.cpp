55cf38675b4ada3d4e2425c00a41fdacce537caf
manojrameshrao <manojrameshrao@gmail.com>
2017-10-30 00:33:57 -0700

    Adding motor cases based on speed and direction

--- Motor_Viserion/L5_Application/periodic_scheduler/period_callbacks.cpp
+++ Motor_Viserion/L5_Application/periodic_scheduler/period_callbacks.cpp
@@ -28,7 +28,6 @@
  * do must be completed within 1ms.  Running over the time slot will reset the system.
  */
 
-
 #include <stdint.h>
 #include "io.hpp"
 #include "periodic_callback.h"
@@ -40,7 +39,12 @@
 #include "dbc_app_send_can_msg.h"
 #include "send_motor_heartbeat.h"
 #include "receive_heartbeats.h"
+#include "lpc_pwm.hpp"
 
+MASTER_SPEED_t speed = { 0 };
+
+PWM motor_speed(PWM::pwm1, 10);
+PWM motor_dir(PWM::pwm2, 10);
 
 /// This is the stack size used for each of the period tasks (1Hz, 10Hz, 100Hz, and 1000Hz)
 const uint32_t PERIOD_TASKS_STACK_SIZE_BYTES = (512 * 4);
@@ -67,9 +71,11 @@ void data_ovr(uint32_t)
 
 bool period_init(void)
 {
-	CAN_init(can1,100,100,100,bus_off,data_ovr);
-	CAN_bypass_filter_accept_all_msgs();
-	CAN_reset_bus(can1);
+    CAN_init(can1, 100, 100, 100, bus_off, data_ovr);
+    CAN_bypass_filter_accept_all_msgs();
+    CAN_reset_bus(can1);
+    motor_speed.set(19.7);
+    motor_dir.set(18);
     return true; // Must return true upon success
 }
 
@@ -87,21 +93,91 @@ bool period_reg_tlm(void)
 
 void period_1Hz(uint32_t count)
 {
-	if(CAN_is_bus_off(can1))
-	    CAN_reset_bus(can1);
+    if (CAN_is_bus_off(can1)) CAN_reset_bus(can1);
 
-    send_motor_heartbeat();
+    //motor_speed.set(19.5);
+    //LE.on(3);
+    //send_motor_heartbeat();
+    //receive_heartbeats();
 }
 
 void period_10Hz(uint32_t count)
 {
-	receive_heartbeats();
-   // LE.toggle(2);
+
+    // LE.toggle(2);
 }
 
 void period_100Hz(uint32_t count)
 {
-	// LE.toggle(3);
+    can_msg_t motor_message;
+    dbc_msg_hdr_t can_msg_hdr;
+    //motor_speed.set(19);
+
+    while (CAN_rx(can1, &motor_message, 0)) {
+
+        can_msg_hdr.dlc = motor_message.frame_fields.data_len;
+        can_msg_hdr.mid = motor_message.msg_id;
+
+        if (motor_message.msg_id == 203) {
+            //dbc_decode_MASTER_DIRECTION(&dir, motor_message.data.bytes, &can_msg_hdr);
+            dbc_decode_MASTER_SPEED(&speed, motor_message.data.bytes, &can_msg_hdr);
+            switch (speed.MASTER_Maintain_Speed) {
+                case 0:
+                    //reverse
+                {
+                    float value3 = 0;
+                    motor_speed.set(value3);
+                    value3 -= 0.1;
+                    if (value3 <= 16.5) value3 = 16.5;
+                }
+                    break;
+                case 1:
+                    //brakes
+                    motor_speed.set(12);
+                    LE.on(1);
+                    break;
+                case 2:
+                    //forward slow
+                    motor_speed.set(19.7);
+                    break;
+                case 3:
+                    //fast
+                    motor_speed.set(20);
+                    break;
+                case 4:
+                    //turbo
+                    break;
+                default:
+                    break;
+                    //MIA
+                }
+
+            switch (speed.MASTER_Maintain_Direction) {
+                case 0:
+                    //center
+                    motor_dir.set(18);
+                    break;
+                case 1:
+                    //hard left 10.9
+                    motor_dir.set(10.9);
+                    break;
+                case 2:
+                    //slight-left 14.9
+                    motor_dir.set(14.9);
+                    break;
+                case 3:
+                    //hard right 25
+                    motor_dir.set(25);
+                    break;
+                case 4:
+                    //slight right 20.7
+                    motor_dir.set(20.7);
+                    break;
+            }
+        }
+
+    }
+
 }
 
 // 1Khz (1ms) is only run if Periodic Dispatcher was configured to run it at main():
