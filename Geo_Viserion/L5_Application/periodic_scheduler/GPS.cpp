f7cb6d2f742ce78e772c3f21011589805582977c
JeanMaryM <jeanmary31ece@gmail.com>
2017-12-04 02:54:49 -0800

    GEO_JM: Added bearing angle calculation logic

--- Geo_Viserion/L5_Application/periodic_scheduler/GPS.cpp
+++ Geo_Viserion/L5_Application/periodic_scheduler/GPS.cpp
@@ -5,38 +5,39 @@
  *  Created on: Oct 29, 2017
  *      Author: Ajinkya
 **/
-
+ /*Add dependent headers*/
 #include "GPS.h"
-#include "io.hpp"
+
+/*Global definitions*/
 char buffer[200] = {0};
 char satelite[3];
-
 uint8_t no_sat_locked;
 Uart2& GPS_data = Uart2::getInstance();
 cordinates current, projection, checkpoint;
+
  /* Initializes the GPS module
  * No Function parameters
  *
  **/
-//struct cordinates current;
-//, projection, checkpoint;
-//current.latitude = 0;
 void init_GPS_module()
 {
-    GPS_data.init(115200,76,1);//Configure the GPS in pedestrian mode
-   // current.latitude = 0;
+    /*Configure the GPS in pedestrian mode*/
+    GPS_data.init(115200,76,1);
 }
+
+/*Convert the DDDMM.MMMMM format to decimal degrees*/
 double convert_to_degrees(double value)
 {
     double minutes;
     int reference,degrees;
     degrees = value/100;
-    reference = (int)(value / 100) ;
+    reference = (int)(value / 100);
     reference = reference * 100;
     minutes = (value - reference)/60;
     return degrees + minutes;
 }
 
+/*Get current coordinates from GPS module*/
 void get_GPS_data()
 {
     const char delimiter[2] = ",";
@@ -47,7 +48,7 @@ void get_GPS_data()
     char *gps_longitude;
     /* Get the GPS data */
     GPS_data.gets(buffer,sizeof(buffer),0);
-    char buff[200] = "$GNGGA,094727.20,3720.00458,N,12154.72293,W,2,12,0.82,44.8,M,-29.9,M,,0000*41";
+    //char buff[200] = "$GNGGA,094727.20,3720.00458,N,12154.72293,W,2,12,0.82,44.8,M,-29.9,M,,0000*41";
     int i=0;
     /* Byte 47 and 48 holds the number of connected satellites */
     satelite[0]=buffer[46];
@@ -78,7 +79,64 @@ void get_GPS_data()
           break;
       }
     }
+    current.latitude = convert_to_degrees(atof(gps_latitude));
+    current.longitude = convert_to_degrees(atof(gps_longitude));
+    set_projection_data(current.latitude,current.longitude);
     printf ("gps lat:%f\n",convert_to_degrees(atof(gps_latitude)));
     printf ("gps long:%f\n",convert_to_degrees(atof(gps_longitude)));
 }
 
+/*Set projection coordinates based on current coordinates*/
+void set_projection_data(double latitude, double longitude)
+{
+    projection.latitude = latitude + 0.01;
+    projection.longitude = longitude;
+}
+
+/*Set checkpoint coordinates based on bluetooth*/
+void set_checkpoint_data(double latitude, double longitude)
+{
+    checkpoint.latitude = latitude;
+    checkpoint.longitude = longitude;
+}
+
+/*Calculate the bearing angle*/
+float get_bearing_angle()
+{
+    cordinates vector_CD, vector_CP;
+    double dot_product = 0, mod_CD =0, mod_CP=0;
+    float bearing_angle = 0;
+
+    /*Get GPS data and set projection coordinates*/
+    get_GPS_data();
+
+    /*Vector coordinates of car position(current) to its destination (checkpoint)*/
+    vector_CD.latitude = checkpoint.latitude - current.longitude;
+    vector_CD.longitude = checkpoint.longitude - current.longitude;
+
+    /*Vector coordinates of car position(current) to its projection (projection)*/
+    vector_CP.latitude = projection.latitude - current.longitude;
+    vector_CP.longitude = projection.longitude - current.longitude;
+
+    /*Dot product of both Vectors CD & CP (CD . CP)*/
+    dot_product = (vector_CD.latitude * vector_CP.latitude) + (vector_CD.longitude * vector_CP.longitude);
+
+    /*Magnitude of vector_CD (|CD|)*/
+    mod_CD = sqrt(pow(vector_CD.latitude,2) + pow(vector_CD.longitude,2));
+
+    /*Magnitude of vector_CP (|CP|)*/
+    mod_CP = sqrt(pow(vector_CP.latitude,2) + pow(vector_CP.longitude,2));
+
+    /*Computes Arc-Cosine to find bearing angle and converts results to degrees.*/
+    bearing_angle = acos(dot_product/(mod_CD*mod_CP)) * (180.0 / M_PI);
+
+    /*Since range of arccos is [0,PI], we need to check for the position of current and
+     * checkpoint longitudes to make the values in the range of (-180,180]
+     */
+    if(checkpoint.longitude < current.longitude)
+        bearing_angle = bearing_angle*(-1);
+
+    printf("Bearing angle: %f\n", bearing_angle);
+    return bearing_angle;
+
+}
--- Geo_Viserion/L5_Application/periodic_scheduler/GPS.h
+++ Geo_Viserion/L5_Application/periodic_scheduler/GPS.h
@@ -12,6 +12,8 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include "io.hpp"
+#include "math.h"
 struct cordinates{
     double latitude = 0;
     double longitude = 0;
@@ -21,6 +23,7 @@ extern char buffer[200];
 void init_GPS_module();
 void get_GPS_data();
 double convert_to_degrees(double value);
-
-
+void set_projection_data(double latitude, double longitude);
+void set_checkpoint_data(double latitude, double longitude);
+float get_bearing_angle();
 #endif /* L5_APPLICATION_PERIODIC_SCHEDULER_GPS_H_ */
